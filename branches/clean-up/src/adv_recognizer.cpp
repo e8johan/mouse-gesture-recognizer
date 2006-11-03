42d5cae5f917d102b39994ce726cce2e8761bd99
quard8 <quard8@2c227689-8b1f-0410-847e-0d0e51d0f70e>
2006-11-03 15:32:44 +0000

    - added copyrights - code cleanup

--- branches/clean-up/src/adv_recognizer.cpp
+++ branches/clean-up/src/adv_recognizer.cpp
@@ -1,5 +1,44 @@
+/*
+ * This file is part of the mouse gesture package.
+ * Copyright (C) 2006 Johan Thelin <e8johan@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above
+ *     copyright notice, this list of conditions and the
+ *     following disclaimer.
+ *   - Redistributions in binary form must reproduce the
+ *     above copyright notice, this list of conditions and
+ *     the following disclaimer in the documentation and/or
+ *     other materials provided with the distribution.
+ *   - The names of its contributors may be used to endorse
+ *     or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 #include "adv_recognizer.h"
 #include <algorithm>
+
+using namespace Gesture;
 
 RealTimeMouseGestureRecognizer::RealTimeMouseGestureRecognizer( int minimumMovement, double minimumMatch, bool allowDiagonals )
 : minimumMatch(minimumMatch), allowDiagonals(true)
@@ -90,7 +129,6 @@ struct DirectionSort
 void RealTimeMouseGestureRecognizer::addGestureDefinition( const GestureDefinition &gesture )
 {
 	gestures.push_back( gesture );
-
 	std::sort( gestures.begin(), gestures.end(), DirectionSort() );
 }
 
@@ -105,7 +143,6 @@ void RealTimeMouseGestureRecognizer::recognizeGesture()
 	
 	for( GestureList::const_iterator gi = gestures.begin(); gi != gestures.end(); ++gi )
 	{
-
 		int readIndex = directions.getReadPointer();
 		bool match = true;
     
